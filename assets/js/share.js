3f338d83a545cb2ccefa8bf587f89d431f00d9fb
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2024-01-27 20:48:31 +0200

    Change LinkedIn button color. Improve share buttons script.

--- assets/js/share.js
+++ assets/js/share.js
@@ -1,23 +1,28 @@
-var articleUrl = window.location.href;
-var articleTitle = document.getElementsByClassName('article-title')[0].textContent;
-var fbShareButton = document.getElementById('share-button-fb');
-var twShareButton = document.getElementById('share-button-tw');
-var lnShareButton = document.getElementById('share-button-ln');
+// Wait for the window to load
+window.onload = function () {
 
-fbShareButton.addEventListener('click', function() {
+  var articleUrl = window.location.href;
+  var articleTitle = document.querySelectorAll('.article-title')[0].textContent;
+  var fbShareButton = document.querySelectorAll('.facebook')[0];
+  var twShareButton = document.querySelectorAll('.twitter')[0];
+  var lnShareButton = document.querySelectorAll('.linkedin')[0];
+
+  fbShareButton.addEventListener('click', function () {
     window.open('https://www.facebook.com/sharer/sharer.php?u=' + articleUrl,
-        'facebook-share-dialog',
-        'width=800,height=600'
+      'facebook-share-dialog',
+      'width=800,height=600'
     );
     return false;
-});
+  });
 
-twShareButton.addEventListener('click', function() {
+  twShareButton.addEventListener('click', function () {
     window.open(`https://twitter.com/share?url=${articleUrl}`);
     return false;
-});
+  });
 
-lnShareButton.addEventListener('click', function() {
-  window.open(`https://www.linkedin.com/shareArticle?mini=true&url=${articleUrl}&title=${articleTitle}`)
-  return false;
-});
+  lnShareButton.addEventListener('click', function () {
+    window.open(`https://www.linkedin.com/shareArticle?mini=true&url=${articleUrl}&title=${articleTitle}`)
+    return false;
+  });
+
+};
